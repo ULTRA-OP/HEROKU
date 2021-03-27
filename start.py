0c63a48b6c23b3babeae20b55c819760378ad6ef
Caleb Noble <74255178+iam-pro@users.noreply.github.com>
2021-03-27 15:06:36 +0530

    Use Logic Bitch

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
 from os import system # ma ka bhosda
-system ("git clone https://GitHub.com/ULTRA-OP/ULTRA-X && python3 -m ULTRA")
+system ("git clone https://GitHub.com/ULTRA-OP/ULTRA-X && cd ULTRA-X && python3 -m ULTRA")
 # try as phuk
 print("phuk")
