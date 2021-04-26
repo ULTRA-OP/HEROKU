c0429b9775b843ca70ddc1908a8c141eb97e94e9
AGENTOP47 <83199211+AGENTOP47@users.noreply.github.com>
2021-04-26 06:04:52 +0000

    Update start.py

--- start.py
+++ start.py
@@ -1,5 +1,5 @@
 from os import system # chala ja bhosdike madarchod
-system ("git clone https://GitHub.com/ULTRA-OP/ULTRA-X && cd ULTRA-X && python3 -m ULTRA")
+system ("git clone https://GitHub.com/AGENTOP47/AGENT47-BOT && cd AGENT47-BOT && python3 -m ULTRA")
 # try af
 print("OH SYSTEM IS CRASHED")
 system("python3 -m ULTRA")
