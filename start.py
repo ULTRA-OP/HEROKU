0ab502368e599969b4f65717fc579c64a9fce1ad
PROBOY X <68194873+PROBOYX@users.noreply.github.com>
2021-05-09 10:23:21 +0530

    DONE

--- start.py
+++ start.py
@@ -1,6 +1,4 @@
 from os import system # chala ja bhosdike madarchod
 system ("git clone https://GitHub.com/ULTRA-OP/ULTRA-X && cd ULTRA-X && python3 -m ULTRA")
 # try af
-print("OH SYSTEM IS CRASHED")
-system("python3 -m ULTRA")
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @UltraXchaT")
