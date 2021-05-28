f4aef2b5a5096fda155a7bed2cfdabc6bb924136
B LAC JASS <73884202+JASS697@users.noreply.github.com>
2021-05-29 00:13:56 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
 from os import system # chala ja bhosdike madarchod
-system ("git clone https://github.com/JASS697/JATT-OP-BOT && cd ULTRA-X && python3 -m JATT-OP-BOT")
+system ("git clone https://github.com/JASS697/JATT-OP-BOT && cd ULTRA-X && python3 -m JASS697")
 # try af
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @UltraXchaT")
